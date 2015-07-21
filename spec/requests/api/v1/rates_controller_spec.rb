50b1b0e4af3f053d1e857eb5378d0ab64c052af4
Wesley Conde <kerponeis@gmail.com>
2015-07-21 14:01:37 -0300

    test post, put and patch to routing_erro

--- spec/requests/api/v1/rates_controller_spec.rb
+++ spec/requests/api/v1/rates_controller_spec.rb
@@ -1,8 +1,14 @@
 require "spec_helper"
 
 RSpec.describe Api::V1::RatesController, :type => :controller do
-    it "should get rout" do
+    it "/api/v1/*path" do
       expect(get: '/api/v1/rota_qualquer').to route_to(
+        controller: 'api/v1/rates', action: 'routing_error', path: 'rota_qualquer')
+      expect(post: '/api/v1/rota_qualquer').to route_to(
+        controller: 'api/v1/rates', action: 'routing_error', path: 'rota_qualquer')
+      expect(put: '/api/v1/rota_qualquer').to route_to(
+        controller: 'api/v1/rates', action: 'routing_error', path: 'rota_qualquer')
+      expect(patch: '/api/v1/rota_qualquer').to route_to(
         controller: 'api/v1/rates', action: 'routing_error', path: 'rota_qualquer')
     end
 end
